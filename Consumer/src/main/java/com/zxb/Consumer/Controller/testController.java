3dd6fe5112f2cbff45946448228028dbaf41785b
01Hank <1724818707@qq.com>
2019-08-10 10:38:03 +0800

    更新多数据源模块

--- Consumer/src/main/java/com/zxb/Consumer/Controller/testController.java
+++ Consumer/src/main/java/com/zxb/Consumer/Controller/testController.java
@@ -2,6 +2,7 @@ package com.zxb.Consumer.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class testController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @CrossOrigin("*")
     @GetMapping("/getTest")
     public  String  sayHello(){
         return restTemplate.getForObject("http://localhost:8091/test/hello",String.class);
